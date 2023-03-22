a4916ea85408f01f46c3e11f7f7f7c24d6df1f67
Richie <r2r0m0c0@pop-os.localdomain>
2023-03-21 23:11:24 -0400

    added encryption suport Improved ZstdCompressor seting and added TODO

--- Archiver.py
+++ Archiver.py
@@ -1,14 +1,22 @@
 import tarfile
 from zstandard import ZstdCompressor
 from io import BytesIO
+from cryptography.fernet import Fernet
 
-source_dir = "./DIR"
+source_dir = "./TestDIR"
 
 tar_bytes = BytesIO()
 with tarfile.open(fileobj=tar_bytes, mode="w:tar") as tar:
 	tar.add(source_dir)
 
-compressed_data = ZstdCompressor(level=22).compress(tar_bytes.getvalue())
+compressed_data = ZstdCompressor(level=22, write_checksum=True, write_content_size=True, threads=8).compress(tar_bytes.getvalue())
 
-with open("archive.tar.zst", "wb") as f:
-	f.write(compressed_data)
+key = b"FADvU5pm87FM1ImNeyAK3ZqNRLlqIAKNKmaYHnt5mVU="
+encrypted_bytes = Fernet(key).encrypt(compressed_data)
+
+with open("./archive.tar.zst.encrypted", "wb") as f:
+    f.write(encrypted_bytes)
+
+# TODO
+# Upload file to azure
+# Hash the output file and save?
