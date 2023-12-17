83964c777a2b48b0162cd0c5fd5066b0cf78bd1d
Richie Cahill <richie@tmmworkshop.com>
2023-12-17 17:53:50 -0500

    Add Advent of Code Day 9 solutions

---
+++ Advent_of_code/2023/DAY9_Part1.py
@@ -0,0 +1,62 @@
+"""DAY7_Part1"""
+from pathlib import Path
+
+
+def clean_data(input_data: list[str]) -> list[list[int]]:
+    """Clean data"""
+    output_data = []
+    for line in input_data:
+        output_line = [int(char) for char in line.split(" ")]
+        output_data.append(output_line[::-1])
+    return output_data
+
+
+def calculate_maps(input_data: list[list[int]]) -> list[list[int]]:
+    """calculate_maps"""
+    output_data = []
+    max_iter = 100
+    for item_1 in input_data:
+        test = [item_1]
+        count = 0
+        while set(test[-1]) != {0}:
+            temp = []
+            for index, item in enumerate(test[-1]):
+                if index + 1 == len(test[-1]):
+                    break
+                temp.append(test[-1][index + 1] - item)
+            test.append(temp)
+
+            count += 1
+            if count >= max_iter:
+                break
+        test.sort(key=lambda x: len(x))
+        output_data.append(test)
+
+    return output_data
+
+
+def get_history(change_map: list[int]) -> int:
+    """get_history"""
+    for index, row in enumerate(change_map):
+        if index + 1 == len(change_map):
+            return row[-1]
+        thing = row[-1] + change_map[index + 1][-1]
+        change_map[index + 1].append(thing)
+    return 0
+
+
+def main():
+    input_file = Path("./Advent_of_code/2023/DAY9_Part2.txt")
+    with input_file.open("r") as file:
+        input_data = [line.strip() for line in file]
+
+    input_data = clean_data(input_data)
+
+    change_maps = calculate_maps(input_data)
+
+    output = sum([get_history(change_map) for change_map in change_maps])
+
+    print(output)
+
+
+main()
