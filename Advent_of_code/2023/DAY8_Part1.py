64ab12de4b21c6856e46a8df90430f68416e2718
Richie Cahill <richie@tmmworkshop.com>
2023-12-16 21:47:34 -0500

    Add solution for Advent of Code Day 8 Part 1

---
+++ Advent_of_code/2023/DAY8_Part1.py
@@ -0,0 +1,46 @@
+"""DAY7_Part1"""
+from pathlib import Path
+from pprint import pprint
+from time import sleep
+
+
+def create_map(input_data):
+    """create_map"""
+    search_map = {}
+    for line in input_data:
+        id, path = line.split(" = ")
+
+        left, right = path.strip("()").split(", ")
+
+        search_map[id] = {
+            "L": left,
+            "R": right,
+        }
+    return search_map
+
+
+def main():
+    input_file = Path("./Advent_of_code/2023/DAY8_Part1.txt")
+    with input_file.open("r") as file:
+        input_data = [line.strip() for line in file]
+
+    instruction = input_data.pop(0)
+    input_data.pop(0)
+
+    pprint(instruction)
+    search_map = create_map(input_data)
+
+    instruction_len = len(instruction)
+
+    count = 0
+    position = "AAA"
+    while position != "ZZZ":
+        current_instruction = instruction[count % instruction_len]
+        position = search_map.get(position).get(current_instruction)
+
+        count += 1
+
+    print(count)
+
+
+main()
