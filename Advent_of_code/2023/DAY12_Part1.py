6c01087793642e3bc2029202180f9b22b27c8392
Richie Cahill <richie@tmmworkshop.com>
2023-12-26 22:39:34 -0500

    Add DAY12_Part1.py for Advent of Code 2023

---
+++ Advent_of_code/2023/DAY12_Part1.py
@@ -0,0 +1,68 @@
+"""DAY12_Part1"""
+
+from pathlib import Path
+
+
+def is_valid(letters: list[str], groups: list[int]) -> bool:
+    """Checks if the given arrangement satisfies the group constraints.
+
+    Args:
+        letters (list[str]): The arrangement of characters.
+        groups (list[int]): The list of group sizes.
+
+    Returns:
+        bool: True if the arrangement satisfies the group constraints, False otherwise.
+    """
+    idx = 0
+    for group in groups:
+        idx = letters.find("#", idx)
+        if idx == -1 or idx + group > len(letters) or letters[idx : idx + group] != "#" * group:
+            return False
+        idx += group
+        if idx < len(letters) and letters[idx] != ".":
+            return False
+    return "#" not in letters[idx:]
+
+
+def backtrack(letters: list[str], pos: int, groups: list[int]) -> int:
+    """Explores all valid arrangements using backtracking.
+
+    Args:
+        letters (list[str]): The list of letters representing the arrangement.
+        pos (int): The current position in the arrangement.
+        groups (list[int]): The list of group sizes.
+
+    Returns:
+        int: The number of valid arrangements.
+    """
+    if pos == len(letters):
+        return 1 if is_valid("".join(letters), groups) else 0
+
+    if letters[pos] != "?":
+        return backtrack(letters, pos + 1, groups)
+
+    count = 0
+    for state in ["#", "."]:
+        letters[pos] = state
+        count += backtrack(letters, pos + 1, groups)
+    letters[pos] = "?"
+    return count
+
+
+def main() -> None:
+    """Main function to process input file and count arrangements."""
+    input_file = Path("./Advent_of_code/2023/DAY12_Part1.txt")
+    input_data = input_file.read_text().splitlines()
+
+    result = 0
+    for line in input_data:
+        split_line = line.split(" ")
+        letters = list(split_line[0])
+        numbers = [int(num) for num in split_line[1].split(",")]
+        result += backtrack(letters=letters, pos=0, groups=numbers)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
